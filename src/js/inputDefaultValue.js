f3e126ca13c516dad54724956d7ce474857324ca
annssva <curlyannas0@gmail.com>
2023-08-28 15:24:27 +0300

    commit

--- src/js/inputDefaultValue.js
+++ src/js/inputDefaultValue.js
@@ -1,21 +1,24 @@
-// Функции для значений полей ввода по умолчанию
+
+export const defaultValueHandle = () => {
+    // Функции для значений полей ввода по умолчанию
 // Получение всех элементов с селектором ".formInput"
-const inputFields = document.querySelectorAll(".formInput");
+    const inputFields = document.querySelectorAll(".formInput");
 
 // Добавление листинера к каждому элементу
-inputFields.forEach((input) => {
-    input.addEventListener("focus", function () {
-        // Проверяем, равно ли значение исходному значению, и если да, устанавливаем в value пустую строку
-        if (this.value === this.getAttribute("value")) {
-            this.value = "";
-        }
-    });
+    inputFields.forEach((input) => {
+        input.addEventListener("focus", function () {
+            // Проверяем, равно ли значение исходному значению, и если да, устанавливаем в value пустую строку
+            if (this.value === this.getAttribute("value")) {
+                this.value = "";
+            }
+        });
 
-    // Добавляем листенер событий, когда ввод теряет фокус
-    input.addEventListener("blur", function () {
-        // Проверяем, если поле ввода пустое, то возвращаем изначальное значение value
-        if (this.value === "") {
-            this.value = this.getAttribute("value");
-        }
+        // Добавляем листенер событий, когда ввод теряет фокус
+        input.addEventListener("blur", function () {
+            // Проверяем, если поле ввода пустое, то возвращаем изначальное значение value
+            if (this.value === "") {
+                this.value = this.getAttribute("value");
+            }
+        });
     });
-});
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import '../scss/style.scss'
+import{ defaultValueHandle } from "./inputDefaultValue.js";
+
+defaultValueHandle()
 
 
 
